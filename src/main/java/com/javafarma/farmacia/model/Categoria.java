24fa32b41458e3571ece1ec3c8e7ae428b11a172
felipe-jaque-dev <felipealexandrej@gmail.com>
2024-01-04 14:11:21 -0300

    Adicionado Classes de Produto, Relacionamento entre classe produto e categoria, adicionado Anotações Column no atributo nome(classe categoria) para evitar problemas com a classe Categoria.

--- src/main/java/com/javafarma/farmacia/model/Categoria.java
+++ src/main/java/com/javafarma/farmacia/model/Categoria.java
@@ -1,9 +1,17 @@
 package com.javafarma.farmacia.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -16,13 +24,19 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@NotBlank(message = "O atributo nome é obrigatorio!")
-	@Size(min = 1, max = 100, message = "O atributo Nome deve conter no mínimo 1 caractere!")
+	@Size(min = 1, max = 100, message = "O atributo Nome deve conter no mínimo 1 e no maximo 100 caracteres!")
+	@Column(length = 100)
 	private String nome;
-	
+
 	@NotNull(message = "O atributo descrição é obrigatorio")
 	private String descricao;
+
+	// Relacionamento com Tabela de Produtos
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "categoria", cascade = CascadeType.REMOVE)
+	@JsonIgnoreProperties("categoria")
+	private List<Produto> produto;
 
 	public Long getId() {
 		return id;
@@ -47,7 +61,13 @@ public class Categoria {
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
-	
-	
-	
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+
 }
